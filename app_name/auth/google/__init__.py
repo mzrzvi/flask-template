d86954299bf5e8797f3ae998717a2c4dd48d6889
Moiz Rizvi <moizrizvi@gmail.com>
2019-02-06 12:08:37 -0600

    [Fix] Remove flask-dance

--- app_name/auth/google/__init__.py
+++ app_name/auth/google/__init__.py
@@ -1,15 +1,3 @@
 """
 Google auth module
 """
-
-from app_name import app
-
-from flask_dance.contrib.google import make_google_blueprint
-
-blueprint = make_google_blueprint(
-    client_id=app.config.get('GOOGLE_CLIENT_ID'),
-    client_secret=app.config.get('GOOGLE_CLIENT_SECRET'),
-    scope=["profile", "email"]
-)
-
-app.register_blueprint(blueprint, url_prefix="/login")
